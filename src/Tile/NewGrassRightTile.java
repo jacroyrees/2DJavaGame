7d391ed2132df9dc68e813a8ed72a744d53fd5d6
Jac Rees <jacroyrees@outlook.com>
2020-04-29 20:56:00 +0100

    Added Custom  Tiles -> Stupid way of implementing the tiles but there we are...

---
+++ src/Tile/NewGrassRightTile.java
@@ -0,0 +1,10 @@
+package Tile;
+
+import graphics.Assets;
+
+public class NewGrassLeftTile extends Tile{
+    public NewGrassLeftTile(int id) {
+        super(Assets.newDirt.get(1), id);
+    }
+
+}
