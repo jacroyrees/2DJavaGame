dcc635acce49dc13509686e0fefe02a4121439f2
Jozef <=>
2020-05-01 13:36:41 +0100

    updated the static tiles to work with a slightly better system (still needs an overhaul)

--- src/Tile/Tile.java
+++ src/Tile/Tile.java
@@ -4,6 +4,7 @@ import Tile.DirtTiles.DirtTile;
 import Tile.GrassTiles.*;
 import Tile.StoneTiles.SandStoneTile;
 import Tile.StoneTiles.StoneTile;
+import Utilities.Edge;
 import graphics.Assets;
 
 import java.awt.*;
@@ -67,7 +68,7 @@ public abstract class Tile {
 
     }
 
-    public static Tile stone = new StoneTile(00);
+/*    public static Tile stone = new StoneTile(00);
     public static Tile DirtTile = new DirtTile(15);
 
     public static Tile grassBottomLeft = new GrassBottomLeftTile(10);
@@ -78,6 +79,19 @@ public abstract class Tile {
     public static Tile grassTopRight = new GrassTopRightTile(14);
     public static Tile grassTopTile = new GrassTopTile(19);
     public static Tile grassBottomTile = new GrassBottomTile(11);
+    public static Tile grassTile = new GrassTile(18);*/
+
+    public static Tile stone = new StoneTile(00);
+    public static Tile DirtTile = new DirtTile(15);
+
+    public static Tile grassBottomLeft = new GrassTile(10, Edge.LEFT);
+    public static Tile grassBottomRight = new GrassTile(12, Edge.RIGHT);
+    public static Tile grassLeft = new GrassTile(15, Edge.LEFT);
+    public static Tile grassRight = new GrassTile(17, Edge.RIGHT);
+    public static Tile grassTopLeft = new GrassTile(13, Edge.TOPLEFT);
+    public static Tile grassTopRight = new GrassTile(14, Edge.TOPRIGHT);
+    public static Tile grassTopTile = new GrassTile(19, Edge.TOP);
+    public static Tile grassBottomTile = new GrassTile(11, Edge.BOTTOMRIGHT);
     public static Tile grassTile = new GrassTile(18);
 
 
